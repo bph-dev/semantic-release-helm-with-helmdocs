05285ba10fe444fd86bcb9dd4dc4b7b4ce56e889
bph-dev <bphillips.swe@gmail.com>
2023-02-01 11:24:32 -0500

    feat(upgrade): Remove whitespaces, installed lodash for utility methods, and added more logging.

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 import verifyChart from './lib/verifyConditions.js';
 import verifyHelmDocsInstallation from './lib/verifyHelmDocsInstallation.js';
 import prepareChart from './lib/prepare.js';
-import prepareHelmChartDocumentation from   './lib/render.js';
+import prepareHelmChartDocumentation from './lib/render.js';
 import publishChart from './lib/publish.js';
+
+import { isNil, isEmpty, isUndefined, isBoolean } from 'lodash';
 
 let verified = false;
 let verifiedInstallation = false;
 let prepared = false;
 
 async function verifyConditions(pluginConfig, context) {
+  const logger = context.logger;
+
 	await verifyChart(pluginConfig, context);
-	
-  verifiedInstallation = verifyHelmDocsInstallation(pluginConfig, context); 
+
+  if (!isNil(pluginConfig.prepareHelmChartDocs) || !isEmpty(pluginConfig.prepareHelmChartDocs)) {
+    if (isBoolean(pluginConfig.prepareHelmChartDocs)) {
+      verifiedInstallation = verifyHelmDocsInstallation(pluginConfig, context);
+    } else {
+      logger.error('prepareHelmChartDocs must be a boolean');
+      // verifiedInstallation remains false
+    }
+  }
+
   verified = true;
 }
 
@@ -22,11 +34,13 @@ async function prepare(pluginConfig, context) {
     await verifyConditions(pluginConfig, context);
   }
 
-  await prepareChart(pluginConfig, context);		
+  await prepareChart(pluginConfig, context);
 	
-	if (verifiedInstallation) {
-		await prepareHelmChartDocumentation(pluginConfig, context);
-	}
+	if (!isNil(verifiedInstallation) || !isUndefined(verifiedInstallation)) {
+    if (verifiedInstallation) {
+      await prepareHelmChartDocumentation(pluginConfig, context);
+    }
+  }
 
   prepared = true;
 }
